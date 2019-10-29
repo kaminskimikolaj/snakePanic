7dfbac0b51a9845f75769901e44fedf75dff276c
Mikołaj Kamiński <mikolajkaminski1@gmail.com>
2019-10-29 14:17:32 +0100

    Initial Commit

---
+++ snakePanic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snakePanic
+//
+//  Created by Mikołaj Kamiński on 29/10/2019.
+//  Copyright © 2019 Mikołaj Kamiński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
