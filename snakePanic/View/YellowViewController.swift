9f65d635833e19ab061d3b3339acae09b3a2d5c9
Mikołaj Kamiński <mikolajkaminski1@gmail.com>
2019-12-08 22:17:16 +0100

    working prototype2

--- snakePanic/View/YellowViewController.swift
+++ snakePanic/View/YellowViewController.swift
@@ -10,76 +10,91 @@ import UIKit
 
 class YellowViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+//    override func viewDidLoad() {
         
-        let scroll = UIScrollView(frame: .zero)
-        
-        view.addSubview(scroll)
-        scroll.backgroundColor = .systemPink
-        scroll.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            scroll.topAnchor.constraint(equalTo: view.topAnchor),
-            scroll.heightAnchor.constraint(equalToConstant: 300),
-            scroll.widthAnchor.constraint(equalToConstant: 300)
-        ])
-        
-        
-        scroll.contentSize = CGSize(width: 1000, height: 1000)
-        
-        
-        let subview = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 320, height: 800))
-        subview.backgroundColor = .systemGreen
-        scroll.addSubview(subview)
-        
-        let box1 = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
-        box1.backgroundColor = .systemTeal
-        subview.addSubview(box1)
-        
-        let box2 = UIView(frame: CGRect(x: 500, y: 200, width: 100, height: 100))
-        box2.backgroundColor = .systemTeal
-        subview.addSubview(box2)
-    
+//        super.viewDidLoad()
+//
+//        let scroll = UIScrollView(frame: view.safeAreaLayoutGuide.layoutFrame)
+//        view.addSubview(scroll)
+//
 //        let subview = UIView(frame: .zero)
 //        subview.backgroundColor = .systemTeal
+//        scroll.addSubview(subview)
 //        subview.translatesAutoresizingMaskIntoConstraints = false
 //        NSLayoutConstraint.activate([
-//            subview.topAnchor.constraint(equalTo: view.topAnchor),
-//            subview.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 2),
-//            subview.widthAnchor.constraint(equalTo: view.widthAnchor)
+//            subview.topAnchor.constraint(equalTo: scroll.topAnchor),
+//            subview.heightAnchor.constraint(equalTo: scroll.heightAnchor, multiplier: 2),
+//            subview.widthAnchor.constraint(equalTo: scroll.widthAnchor),
+//            subview.leftAnchor.constraint(equalTo: scroll.leftAnchor)
 //        ])
-//        scroll.addSubview(subview)
+//        print(subview.bounds.size)
 //        scroll.contentSize = subview.bounds.size
-        
-//        view.backgroundColor = .systemYellow
-//        let scroll = UIScrollView(frame: .zero)
-//        scroll.backgroundColor = .systemGreen
-//        view.addSubview(scroll)
-//        scroll.contentSize = CGSize(width: view.frame.width, height: view.frame.height * 2)
-//        scroll.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            scroll.topAnchor.constraint(equalTo: view.topAnchor),
-//            scroll.heightAnchor.constraint(equalTo: view.heightAnchor),
-//            scroll.widthAnchor.constraint(equalTo: view.widthAnchor)
-//        ])
-//        let box = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
-//        box.backgroundColor = .systemRed
-//        scroll.addSubview(box)
-//        box.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//           box.bottomAnchor.constraint(equalTo: scroll.bottomAnchor)
-//       ])
 //
-//        let box2 = UIView(frame: .zero)
-//        box2.backgroundColor = .systemBlue
-//        scroll.addSubview(box2)
-//        box2.translatesAutoresizingMaskIntoConstraints = false
+//        let bar1 = UIView(frame: .zero)
+//        bar1.backgroundColor = .systemGreen
+//        subview.addSubview(bar1)
+//        bar1.translatesAutoresizingMaskIntoConstraints = false
 //        NSLayoutConstraint.activate([
-//            box2.bottomAnchor.constraint(equalTo: scroll.bottomAnchor),
-//            box2.heightAnchor.constraint(equalTo: scroll.heightAnchor, multiplier: 1/10),
-//            box2.widthAnchor.constraint(equalTo: scroll.widthAnchor)
+//            bar1.topAnchor.constraint(equalTo: subview.topAnchor),
+//            bar1.heightAnchor.constraint(equalTo: subview.heightAnchor, multiplier: 1/10),
+//            bar1.widthAnchor.constraint(equalTo: subview.widthAnchor)
 //        ])
-        // Do any additional setup after loading the view.
+//
+//        let bar2 = UIView(frame: .zero)
+//        bar2.backgroundColor = .systemBlue
+//        subview.addSubview(bar2)
+//        bar2.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            bar2.bottomAnchor.constraint(equalTo: subview.bottomAnchor),
+//            bar2.heightAnchor.constraint(equalTo: subview.heightAnchor, multiplier: 1/10),
+//            bar2.widthAnchor.constraint(equalTo: subview.widthAnchor)
+//        ])
+//
+//    }
+
+    let setup: UILabel = {
+        let label = UILabel()
+//        label.text = "Scroll Bottom"
+//        label.backgroundColor = .green
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let scrollView: UIScrollView = {
+        let v = UIScrollView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.backgroundColor = .systemOrange
+        return v
+    }()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(scrollView)
+        scrollView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+
+        scrollView.addSubview(setup)
+        let frame = view.safeAreaLayoutGuide.layoutFrame
+        let constant = frame.height * 2
+        setup.leftAnchor.constraint(equalTo: scrollView.leftAnchor).isActive = true
+        setup.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: constant).isActive = true
+        setup.rightAnchor.constraint(equalTo: scrollView.rightAnchor).isActive = true
+        setup.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        
+        let subview = UIView(frame: .zero)
+        subview.backgroundColor = .systemTeal
+        scrollView.addSubview(subview)
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            subview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            subview.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 2),
+            subview.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+            subview.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor)
+        ])
     }
 
 }
